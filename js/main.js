16cad23543d44523e1085923605dbac66dc63d6e
unknown <hebamarzouk17@gmail.com>
2022-03-14 00:40:24 +0200

    Fetching data and add it to html

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Get the elements
+const adviceNum = document.getElementById("advice-num");
+const adviceContent = document.getElementById("advice-content");
+const generatorBtn = document.getElementById("generator");
+
+// make the request
+const adviceGenerator = async () => {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data.slip);
+    // add the data the elements
+    adviceNum.innerText = `# ${data.slip.id}`;
+    adviceContent.innerText = data.slip.advice;
+  } catch (err) {
+    console.log(err);
+  }
+};
+// excute the function on every refresh too
+adviceGenerator();
+
+// add event listener
+generatorBtn.addEventListener("click", adviceGenerator);
